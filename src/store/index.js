f7b375d54e15bab32039f357abd8e8580826e6cb
ericnshimiye <ericnsh@gmail.com>
2019-11-17 19:02:41 -0500

    Add sign in to vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,61 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
+const defaultSigninError = {
+  email: null,
+  password: null
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    signinIn: false,
+    signinError: { ...defaultSigninError }
+  },
+  getters: {
+    signingIn: state => {
+      return state.signinIn;
+    },
+    signinErrors: state => {
+      return state.signinError;
+    }
+  },
+  mutations: {
+    signinStart: state => {
+      state.signinIn = true;
+    },
+    signinStop: (state, errorMessage) => {
+      state.signinIn = false;
+      state.signinError = { ...defaultSigninError, ...errorMessage };
+    }
+  },
+  actions: {
+    signIn({ commit }, signinData) {
+      commit('signinStart');
+
+      const promise = axios.post(
+        'https://labyrinth-api.herokuapp.com/api/users/signin',
+        {
+          ...signinData
+        }
+      );
+
+      promise
+        .then(response => {
+          localStorage.setItem('accessToken', response.data.token);
+          commit('signinStop', null);
+        })
+        .catch(error => {
+          commit('signinStop', error.response.data);
+        });
+
+      return promise;
+    },
+    signOut() {
+      localStorage.removeItem('accessToken');
+    }
+  },
   modules: {}
 });
