32efee07a30d0b1de07ca5e00afbd931413102eb
Charles-André  Beaudry <charles-andre.beaudry.1@ulaval.ca>
2019-11-23 14:36:37 -0500

    Début du lobby

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,70 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <KeyBoardDirectionListener />
+    <div class="header">
+      <h1>Bienvenue au Labyrinthe</h1>
+      <h2>Veuillez attendre les autres joueurs</h2>
+    </div>
+
+    <div class="users">
+      <div v-for="(player, index) of players" v-bind:key="index" class="user">
+        <img v-bind:src="images[index]" class="avatar" />
+        <big>Joueur {{ player.id + 1 }}</big>
+        <IsReady v-bind:isReady="player.isLoggedIn" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import KeyBoardDirectionListener from '@/components/KeyBoardDirectionListener.vue';
-
-import db from '../database/db';
-
-db.ref('players/player1')
-  .set({ id: 0, isLoggedIn: true, position: { x: 0, y: 1 } })
-  .then(() => {
-    console.log('user updated!');
-  });
-
+import IsReady from '@/components/IsReady';
 // we can also use `$firebaseRefs.user` to refer to the bound user reference
 //this.$firebaseRefs.user.set(user)
 export default {
   name: 'Home',
-  components: {
-    KeyBoardDirectionListener
-  }
+  components: { IsReady },
+  data: () => ({
+    images: [
+      'https://cdn4.iconfinder.com/data/icons/reaction/32/angry-512.png',
+      'https://clipart.info/images/ccovers/1499793243facebook-wow-emoji-like-png.png',
+      'https://clipart.info/images/ccovers/1499793247facebook-sad-emoji-like-png.png',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAMHO5MPBu6ztKy20_DfCT6fMTtZFZ2MfO0AuhumDliiYPUXUE&s'
+    ],
+    players: [
+      { id: 0, isLoggedIn: false, username: undefined },
+      { id: 1, isLoggedIn: true, username: undefined },
+      { id: 2, isLoggedIn: true, username: undefined },
+      { id: 3, isLoggedIn: false, username: undefined }
+    ]
+  })
 };
 </script>
+
+<style>
+.home {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+}
+
+.users {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex: 1;
+  margin-top: 128px;
+}
+
+.user {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 200px;
+  margin-bottom: 8px;
+}
+
+.avatar {
+  width: 128px;
+  margin-bottom: 8px;
+}
+</style>
